26730d6415ce08361ee846363e649420aa81dd68
Istvan Acs <istvan.acs89@gmail.com>
2019-05-27 18:29:57 +0100

    Styling the game

--- src/Board.js
+++ src/Board.js
@@ -88,6 +88,7 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
     let hasWon = board.every(row => row.every(cell => !cell))
+
     this.setState({board, hasWon});
   }
 
@@ -98,7 +99,14 @@ class Board extends Component {
 
     // if the game is won, just show a winning msg & render nothing else
     if(this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!!</span>
+          </div>
+        </div>
+      )
     }
     // TODO
 
@@ -115,9 +123,15 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     )
   }
 }
