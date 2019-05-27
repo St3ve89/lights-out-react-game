e1eda9a861c8fca9f7b8dae92e0542d4a26aba63
Istvan Acs <istvan.acs89@gmail.com>
2019-05-27 18:50:09 +0100

    Updates

---
+++ precache-manifest.f48aaa4a8f21faec4602ee5ba1975c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b733b1c08d2218b1f5979eaec67359",
+    "url": "/lights-out-react-game/index.html"
+  },
+  {
+    "revision": "8f6403bdc8c0004837f1",
+    "url": "/lights-out-react-game/static/css/main.279b80c5.chunk.css"
+  },
+  {
+    "revision": "dbd9b9d1d09f56a8fe13",
+    "url": "/lights-out-react-game/static/js/2.63a3d80a.chunk.js"
+  },
+  {
+    "revision": "8f6403bdc8c0004837f1",
+    "url": "/lights-out-react-game/static/js/main.010962bc.chunk.js"
+  },
+  {
+    "revision": "f0c0759c3bf3f508ce8c",
+    "url": "/lights-out-react-game/static/js/runtime~main.97e5ed0a.js"
+  }
+]);
